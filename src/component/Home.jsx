d5e8b0e56c5169f6dad8cc0a5511fa108103da47
杨含知 <yanghanzhi@dhcp-10-105-235-224.wireless.northwestern.private>
2018-11-08 21:20:09 -0600

    modify, ready to add database to implement login func

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -146,8 +146,8 @@ export default class Home extends React.Component{
 	render(){
 		return (
 			<div className="container">
-				<Slides />
 				<JumbotronPage/>
+				<Slides />
 				<HomeCards cards={cards}/>
 			</div>
 			
